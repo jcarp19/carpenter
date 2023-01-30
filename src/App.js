44149d1ce8ae22ecee5b0fd6b3f236373db56b1d
jcarp19 <jim@carpenteronline.us>
2023-01-30 07:27:42 -0500

    added reset and base typography

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ function App() {
   return (
     <div className="App">
       <header className="header">
-        
+        <h1 className="title">Jim Carpenter</h1>
         <a className="App-link" href="/natours/index.html" target="_blank" rel="noopener noreferrer">Natours</a>
         <a className='App-link' href="/nexter/index.html" target="_blank" rel="noopener noreferrer">Nexter</a>
         <a className='App-link' href="/trillo/index.html" target="_blank" rel="noopener noreferrer">Trillo</a>
