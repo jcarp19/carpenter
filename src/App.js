a0c9ffafcc97952bd82b4ccc61b351b7553a648d
jcarp19 <jim@carpenteronline.us>
2023-01-30 06:23:42 -0500

    initial push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        
+        <a className="App-link" href="/natours/index.html" target="_blank" rel="noopener noreferrer">Natours</a>
+        <a className='App-link' href="/nexter/index.html" target="_blank" rel="noopener noreferrer">Nexter</a>
+        <a className='App-link' href="/trillo/index.html" target="_blank" rel="noopener noreferrer">Trillo</a>
       </header>
     </div>
   );
