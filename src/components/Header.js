5dcd79e277f9b30d3a491233440d381b03fcc3c2
jcarp19 <jim@carpenteronline.us>
2023-02-02 07:13:44 -0500

    added color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,11 @@
 function Header() {
     return (
         <header className="header">
-            <h1 className="logo"><span className="logo__first">J<span className="logo__underline">im</span></span><span className="logo__last logo__underline">Carpenter</span></h1>
-            <p className="title">JavaScript Web Developer</p>
+            <h1 className="header__logo"><span className="header__logo--first">J<span className="header__logo--underline">im</span></span><span className="header__logo--last header__logo--underline">Carpenter</span></h1>
+            <p className="header__title">JavaScript Web Developer</p>
             <div className="header__contactInfo">
                 <p className="header__emailAddress"><a href="mailto:jim@carpenteronline.us">jim@carpenteronline.us</a></p>
-                <p className="header__telephoneNumber"><a href="tel:2489534315">(248)953-4315</a></p>
+                <p className="header__telephoneNumber"><a href="tel:2489534315">248.953.4315</a></p>
                 <p className="header__linkedIn"><a href="https://www.linkedin.com/in/jcarp19/" target="_blank" rel="noreferrer">LinkedIn</a></p>
             </div>
         {/* <h1 className="title">Jim Carpenter</h1>
